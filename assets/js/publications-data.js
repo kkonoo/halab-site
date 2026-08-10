135de541f6d5f0c6e5c3e9cecc3387698e0f7668
Eunji Ha <eha@knu.ac.kr>
2026-08-10 16:20:27 +0900

    Update publications-data.js

--- assets/js/publications-data.js
+++ assets/js/publications-data.js
@@ -8,55 +8,55 @@ window.PUBLICATIONS = [
   {
     title: "Single-cell spatial mapping of human kidney development implicates the microenvironment in guiding cell fate decisions",
     url: "https://www.nature.com/articles/s41588-026-02665-0",
-    authors: "Jonathan Levinsohn<sup>^</sup>, S. Grindel, Bernhard Dumoulin, Amin Abedini, … <b>Eunji Ha</b>, Konstantin A. Klötzer, Nancy R. Zhang, … Alex J. Hughes, Katalin Susztak<sup>*</sup>",
+    authors: "Jonathan Levinsohn<sup>^</sup>, … <b>Eunji Ha</b>, ..., Katalin Susztak<sup>*</sup>",
     venue: "Nature Genetics", year: "2026"
   },
   {
     title: "ELF3 links kidney function GWAS loci to maladaptive epithelial inflammation",
     url: "https://www.science.org/doi/10.1126/sciadv.aec6741",
-    authors: "Yanjuan Hou<sup>^</sup>, Konstantin Klötzer, Bernhard Dumoulin, <b>Eunji Ha</b>, Hongbo Liu, Dhanunjay Mukhi, Magaiver Andrade-Silva, Xuemei Hu, Siyu Pan, Katalin Susztak<sup>*</sup>",
+    authors: "Yanjuan Hou<sup>^</sup>, Konstantin Klötzer, Bernhard Dumoulin, <b>Eunji Ha</b>, ..., Katalin Susztak<sup>*</sup>",
     venue: "Science Advances", year: "2026"
   },
   {
     title: "Spatial atlas of diabetic kidney disease reveals a B cell-rich subgroup",
     url: "https://www.nature.com/articles/s41586-026-10363-4",
-    authors: "Bernhard Dumoulin<sup>^</sup>, Jonathan Levinsohn, Konstantin A. Kl\u00F6tzer, Chenyu Li, Liran Mao, <b>Eunji Ha</b>, Samer Mohandes, \u2026 TRIDENT consortium &amp; Katalin Susztak<sup>*</sup>",
+    authors: "Bernhard Dumoulin<sup>^</sup>, Jonathan Levinsohn, ..., <b>Eunji Ha</b>, ..., Katalin Susztak<sup>*</sup>",
     venue: "Nature", year: "2026"
   },
   {
     title: "ACLY-Driven Metabolic Reprogramming Promotes Histone Acetylation and Inflammation-Associated Fibrosis in Chronic Kidney Disease",
     url: "https://advanced.onlinelibrary.wiley.com/doi/10.1002/advs.75247",
-    authors: "Chunxiu Du<sup>^</sup>, Dhanunjay Mukhi, Lingzhi Li, Chenyu Li, Siyu Pan, Bernhard Dumoulin, <b>Eunji Ha</b>, … Kathryn E. Wellen, Katalin Susztak<sup>*</sup>",
+    authors: "Chunxiu Du<sup>^</sup>, ..., <b>Eunji Ha</b>, …, Katalin Susztak<sup>*</sup>",
     venue: "Advanced Science", year: "2026"
   },
   {
     title: "The proteogenomic landscape of the human kidney and implications for cardio-kidney-metabolic health",
     url: "https://www.nature.com/articles/s41591-025-03872-8",
-    authors: "Daigoro Hirohama<sup>^</sup>, Jo\u00E3o Fadista<sup>^</sup>, <b>Eunji Ha</b>, Hongbo Liu, Amin Abedini, Jonathan Levinsohn, \u2026 Anil Karihaloo &amp; Katalin Susztak<sup>*</sup>",
+    authors: "Daigoro Hirohama<sup>^</sup>, Jo\u00E3o Fadista<sup>^</sup>, <b>Eunji Ha</b>, ..., Katalin Susztak<sup>*</sup>",
     venue: "Nature Medicine", year: "2025"
   },
   {
     title: "Analysis of individual patient pathway coordination in a cross-species single-cell kidney atlas",
     url: "https://www.nature.com/articles/s41588-025-02285-0",
-    authors: "Konstantin A. Kl\u00F6tzer<sup>^</sup>, Amin Abedini, Shen Li, Michael S. Balzer, Xiujie Liang, Jonathan Levinsohn, <b>Eunji Ha</b>, \u2026 Nancy R. Zhang<sup>*</sup>, Katalin Susztak<sup>*</sup>",
+    authors: "Konstantin A. Kl\u00F6tzer<sup>^</sup>, ..., <b>Eunji Ha</b>, ..., Nancy R. Zhang<sup>*</sup>, Katalin Susztak<sup>*</sup>",
     venue: "Nature Genetics", year: "2025"
   },
   {
     title: "Kidney multiome-based genetic scorecard reveals convergent coding and regulatory variants",
     url: "https://www.science.org/doi/full/10.1126/science.adp4753",
-    authors: "Hongbo Liu<sup>^*</sup>, Amin Abedini, <b>Eunji Ha</b>, Ziyuan Ma, Xin Sheng, Bernhard Dumoulin, Chengxiang Qiu, \u2026 Adriana M. Hung, Katalin Susztak<sup>*</sup>",
+    authors: "Hongbo Liu<sup>^*</sup>, Amin Abedini, <b>Eunji Ha</b>, ..., Katalin Susztak<sup>*</sup>",
     venue: "Science", year: "2025"
   },
   {
     title: "Human genetics identify convergent signals in mitochondrial LACTB-mediated lipid metabolism in cardiovascular-kidney-metabolic syndrome",
     url: "https://www.cell.com/cell-metabolism/abstract/S1550-4131(24)00403-0",
-    authors: "Shen Li<sup>^</sup>, Hongbo Liu, Hailong Hu, <b>Eunji Ha</b>, Praveena Prasad, \u2026 Garret A. FitzGerald, Melanie R. McReynolds, Katalin Susztak<sup>*</sup>",
+    authors: "Shen Li<sup>^</sup>, Hongbo Liu, Hailong Hu, <b>Eunji Ha</b>, ..., Katalin Susztak<sup>*</sup>",
     venue: "Cell Metabolism", year: "2025"
   },
   {
     title: "Higher genetic risk loads confer more diverse manifestations and higher risk of lupus nephritis in systemic lupus erythematosus",
     url: "https://acrjournals.onlinelibrary.wiley.com/doi/full/10.1002/art.42516",
-    authors: "Young-Chang Kwon<sup>^</sup>, <b>Eunji Ha</b><sup>^</sup>, Hyuk-Hee Kwon, Dae Jin Park, Jung-Min Shin, \u2026 Kwangwoo Kim<sup>*</sup>, Sang-Cheol Bae<sup>*</sup>, So-Young Bang<sup>*</sup>",
+    authors: "Young-Chang Kwon<sup>^</sup>, <b>Eunji Ha</b><sup>^</sup>, ..., Kwangwoo Kim<sup>*</sup>, Sang-Cheol Bae<sup>*</sup>, So-Young Bang<sup>*</sup>",
     venue: "Arthritis & Rheumatology", year: "2023"
   },
   {
@@ -68,7 +68,7 @@ window.PUBLICATIONS = [
   {
     title: "Genetic variants shape rheumatoid arthritis-specific transcriptomic features in CD4+ T cells through differential DNA methylation",
     url: "https://scholar.google.co.kr/citations?user=ZQA9ySsAAAAJ&hl=ko",
-    authors: "<b>Eunji Ha</b><sup>^</sup>, So-Young Bang<sup>^</sup>, Jiwoo Lim, Jun Ho Yun, Jeong-Min Kim, \u2026 Bong-Jo Kim<sup>*</sup>, Kwangwoo Kim<sup>*</sup>, Sang-Cheol Bae<sup>*</sup>",
+    authors: "<b>Eunji Ha</b><sup>^</sup>, So-Young Bang<sup>^</sup>, ..., Bong-Jo Kim<sup>*</sup>, Kwangwoo Kim<sup>*</sup>, Sang-Cheol Bae<sup>*</sup>",
     venue: "Annals of the Rheumatic Diseases", year: "2021"
   },
   {
